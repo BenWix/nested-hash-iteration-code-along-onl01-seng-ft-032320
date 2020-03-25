120b6eaec45919dbebb510d475083bb63ec2de7e
Benjamin Wichser <benwichser@gmail.com>
2020-03-25 21:15:03 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,6 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts["Jon Snow"][:favorite_ice_cream_flavors].remove{|i| i =="strawberry"}
+  contacts["Jon Snow"][:favorite_ice_cream_flavors].reject!{|i| i =="strawberry"}
   contacts
 end
